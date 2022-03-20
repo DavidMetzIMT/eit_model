bc4893500b2e6b821e9313b4739987ce3856081b
Jiawei Zhang <jiawei.ryan.zhang@outlook.com>
2022-03-20 23:59:51 +0100

    solver_ai

--- eit_model/solver_ai.py
+++ eit_model/solver_ai.py
@@ -1,3 +1,142 @@
+import numpy as np
+from eit_model.model import EITModel
+from eit_model.plot import format_inputs, plot_2D_EIT_image, plot_EIT_image
+from eit_model.solver_abc import Solver
+from typing import Any
+from eit_model.data import EITData, EITImage
+from eit_ai.train_utils.workspace import AiWorkspace
+from eit_ai.train_utils.metadata import MetaData, reload_metadata
+from eit_ai.raw_data.matlab import MatlabSamples
+from eit_ai.raw_data.raw_samples import reload_samples
+from eit_ai.train_utils.select_workspace import select_workspace
+from dataclasses import dataclass
+
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+class SolverAi(Solver):
+    
+    def __post_init__(self) -> None:
+        
+        self.metadata:MetaData=None
+        self.workspace:AiWorkspace=None
+        self.fwd_model:dict=None
+        self.eit = EITModel()
+         
+    
+    def _custom_preparation(self, model_dirpath:str='')-> tuple[EITImage, EITData]:
+        """Custom preparation of the solver to be ready for reconstruction      
+
+        Returns:
+            tuple[EITImage, EITData]: a reconstructed EIT image and the 
+            corresponding EIT data used for it (random generated, simulated 
+            or loaded...)
+            params[Any]: Reconstruction parameters
+        """
+        logger.info('Preparation of Ai reconstruction: Start...')
+        
+        sim_data = self.initialize()
+        img_rec= self.solve_rec(sim_data)
+        logger.info('Preparation of Ai reconstruction: Done')
+        return img_rec, sim_data
+        
+        
+
+    
+    def _custom_rec(self, data:EITData)-> EITImage:
+        """Custom reconstruction of an EIT image using EIT data/measurements
+
+        Args:
+            data (EITData): EIT data/measurements for the reconstruction 
+
+        Returns:
+            EITImage: a reconstructed EIT image corresponding to the EIT 
+            data/measurements
+        """ 
+        return self.solve_rec(data)
+    
+    
+    def initialize(self, model_dirpath:str='')-> EITData:
+        """initialize the reconstruction method
+
+        Args:
+            model_dirpath (str, optional): Ai model path. Defaults to ''.
+
+        Returns:
+            EITData: _description_
+        """
+        self.ready.clear()
+        
+        self.metadata = reload_metadata(dir_path=model_dirpath)
+        raw_samples= reload_samples(MatlabSamples(),self.metadata)
+        self.workspace= select_workspace(self.metadata)
+        self.workspace.load_model(self.metadata)
+        self.workspace.build_dataset(raw_samples, self.metadata)
+        self.fwd_model=self.workspace.getattr_dataset('fwd_model')
+        voltages, _=self.workspace.extract_samples(
+            dataset_part='test', idx_samples='all')
+        logger.debug(f'{voltages.shape}')
+        perm_real=self.workspace.get_prediction(
+            metadata=self.metadata,
+            single_X=voltages[2],
+            preprocess=False)
+        logger.debug(f'{perm_real= }, {perm_real.shape}')
+        
+        perm=format_inputs(self.fwd_model, perm_real)
+        
+        logger.debug(f'perm shape = {perm.shape}')
+        # tri, pts, data = get_elem_nodal_data(self.fwd_model, perm)
+        # self.eit_model.fem.set_mesh(tri, pts, data['elems_data'])
+        self.ready.set()
+        
+        # img_h=self.eit_model.build_img(data=homogenious_conduct, label='homogenious')
+        # homo = self.metadata.set_4_raw_samples(data_sel=['Xh, Yh'])
+
+        # img_ih=image
+        # if img_ih is None: # create dummy image
+            
+        #     ds= (voltages[:,1] - voltages[:,0]) / voltages[:,0]
+        #     logger.debug(f'{ds=}\n, voltage shape: {voltages.shape}')
+        #     perm=self.workspace.get_prediction(
+        #         metadata=self.metadata,
+        #         single_X=ds,
+        #         preprocess=True)
+            
+        #     img_ih= self.eit_model.build_img(
+        #         data= perm, label='inhomogenious')
+
+        # data_h= self.eit_model.build_meas_data(voltages, voltages)
+        # data_ih= self.eit_model.build_meas_data(voltages, voltages)
+
+        init_data= self.eit_model.build_meas_data(
+            voltages[2],voltages[2], 'solved data')
+        return init_data
+    
+    def solve_rec(self, data:EITData)-> EITImage:
+        """Reconstruction of an EIT image 
+        using EIT data/measurements
+
+        Args:
+            data (EITData): EIT data/measurements for the reconstruction 
+
+        Returns:
+            EITImage: a reconstructed EIT image corresponding to the EIT 
+            data/measurements
+        """ 
+        if self.ready.is_set():
+            ds= (data.frame- data.ref_frame) / data.ref_frame
+
+            logger.debug(f'{ds=}\n, {data =}')
+            perm_real=self.workspace.get_prediction(
+                metadata=self.metadata,
+                single_X=ds,
+                preprocess=True)
+        
+        return self.eit_model.build_img(data= perm_real, label='rec image')
+        
+    
+    
 if __name__ == '__main__':
 
     from matplotlib import pyplot as plt
@@ -6,3 +145,20 @@ if __name__ == '__main__':
     import glob_utils.files.files
     import glob_utils.log.log
     glob_utils.log.log.main_log()
+    
+    eit_mdl= EITModel()
+    eit_mdl.load_defaultmatfile()
+    
+    solver= SolverAi(eit_mdl)
+    img_rec, data_sim=solver.prepare_rec()
+    
+    ref = np.random.randn(1,256)
+    frame = np.random.randn(1, 256)
+    v = eit_mdl.build_meas_data(ref, frame)
+    # img = eit_mdl.build_img(v, 'rec')
+    # print(v)
+    rec= solver.solve_rec(v)
+
+    fig, ax = plt.subplots(1,1)
+    plot_EIT_image(fig, ax, rec)
+    plt.show()
