8bd21c8f43e10ef1ea5d286a1a201ed1b7a925ee
Jiawei Zhang <jiawei.ryan.zhang@outlook.com>
2022-04-11 02:03:20 +0200

    add electrodes

--- eit_model/vista_plot.py
+++ eit_model/vista_plot.py
@@ -4,6 +4,9 @@ import sys
 
 # Setting the Qt bindings for QtPy
 import os
+from turtle import shape
+
+from matplotlib.pyplot import axis
 
 from eit_model.model import EITModel
 from eit_model.plot import format_inputs, get_elem_nodal_data
@@ -72,11 +75,23 @@ class PyVistaPlotWidget(MainWindow):
         self.plotter.clear()
         self.plotter.add_mesh(sphere, show_edges=True)
         self.plotter.reset_camera()
+        
+    def add_electrodes(self):
+        elec_pos=self.eit.fem.elec_pos_orient()[:, :3]
+        elec_label=[str(x+1) for x in range(elec_pos.shape[0])]
+        for i in range (elec_pos.shape[0]):
+            elec_mesh = pv.Sphere(0.25, elec_pos[i])
+            single_electrode = elec_mesh.slice(normal='z',)
+            # electrodes.append(single_electrode)
+            self.plotter.add_mesh(single_electrode, color='green', line_width=10, pickable=True,)
+            self.plotter.add_point_labels(elec_pos, elec_label,font_size=5)
+            self.plotter.reset_camera
+        
     
     def plot_eit(self):
 
         pts, tri = self.eit.fem.nodes, self.eit.fem.elems
-        data= np.random.rand(tri.shape[0], 1)*10
+        # data= np.random.rand(tri.shape[0], 1)*10
         self.data=self.eit.sim["img_ih"]["elem_data"]
 
         # cell must contain padding indicating the number of points in the cell
@@ -86,13 +101,27 @@ class PyVistaPlotWidget(MainWindow):
         cells= _cells.astype(np.int64).flatten()
         cell_type = np.array([vtk.VTK_TETRA]*tri.shape[0], np.int8)
         mesh = pv.UnstructuredGrid(cells, cell_type, pts)
-
+        
         colors = np.real(self.data)
-        # colors[colors == 1] = math.nan
+        logger.debug(f'{colors=}{colors.shape=}')
+        # colors[colors == np.max(colors)] = math.nan
         self.plotter.clear()
-        self.plotter.add_mesh(mesh, show_edges=True, scalars=colors)
+        self.plotter.add_mesh(mesh, show_edges=True, scalars=colors,opacity=0.5)
+    
+        elec_pos=self.eit.fem.elec_pos_orient()[:, :3]
+        elec_label=[str(x+1) for x in range(elec_pos.shape[0])]
+        for i in range (elec_pos.shape[0]):
+            elec_mesh = pv.Sphere(0.25, elec_pos[i], theta_resolution=8, phi_resolution=8)
+            single_electrode = elec_mesh.slice(normal='z',)
+            # electrodes.append(single_electrode)
+            self.plotter.add_mesh(single_electrode, color='green', line_width=10, pickable=True,)
+            self.plotter.add_point_labels(elec_pos, elec_label,font_size=5)
+        
+
+        self.plotter.add_axes()
         self.plotter.reset_camera()
         # self.plotter.background_color='white'
+        
 
 
 
